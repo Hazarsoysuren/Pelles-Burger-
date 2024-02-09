b526e9c33cc5c036b70d5a9039cc25d92591eea3
Hazarsoysuren <hazar.soysuren1@gmail.com>
2024-02-09 04:22:04 +0000

    Added some more content

--- Pelles Burger/src/index.js
+++ Pelles Burger/src/index.js
@@ -1,1 +1,11 @@
- 
+document.addEventListener("DOMContentLoaded", function() {
+    const currentDay = new Date().getDay();
+    const offerImage = document.getElementById("offer-image");
+    const dailyOffers = [
+        "images/bacon.jpg",
+        "images/bbq.jpg",
+        "images/special.jpg",
+        "images/garlic.jpg",
+    ];
+    offerImage.src = dailyOffers[currentDay];
+});
